a7dd56c338e97034442b3f52ecb914205a8aab60
Ary <sehgalary@gmail.com>
2025-10-05 21:01:43 +0300

    Created the confirm email model

---
+++ AryTickets/Models/ConfirmEmailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AryTickets.Models
+{
+    public class ConfirmEmailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
