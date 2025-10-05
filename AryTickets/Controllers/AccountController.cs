702a6069743798b4e178a880e8196273ae0c7d16
Ary <sehgalary@gmail.com>
2025-10-05 21:02:42 +0300

    Updated the AccountController

--- AryTickets/Controllers/AccountController.cs
+++ AryTickets/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using AryTickets.Models;
+using AryTickets.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -9,11 +10,13 @@ namespace AryTickets.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailSender _emailSender;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailSender = emailSender;
         }
 
         [HttpGet]
@@ -36,8 +39,19 @@ namespace AryTickets.Controllers
                     {
                         return RedirectToAction("Index", "Home");
                     }
+                    if (result.IsNotAllowed)
+                    {
+                        ModelState.AddModelError(string.Empty, "You must confirm your email before you can log in.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    }
                 }
-                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                }
             }
             return View(model);
         }
@@ -58,8 +72,12 @@ namespace AryTickets.Controllers
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("Index", "Home");
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
+                        $"<h1>Welcome to AryTix!</h1><p>Please confirm your account by entering this code on our site: <strong>{code}</strong></p>");
+
+                    return View("RegisterConfirmation");
                 }
                 foreach (var error in result.Errors)
                 {
@@ -68,6 +86,35 @@ namespace AryTickets.Controllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult ConfirmEmail(string email)
+        {
+            var model = new ConfirmEmailViewModel { Email = email };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmEmail(ConfirmEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var result = await _userManager.ConfirmEmailAsync(user, model.Code);
+                    if (result.Succeeded)
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                ModelState.AddModelError(string.Empty, "Invalid verification code.");
+            }
+            return View(model);
+        }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
