e9125e9646460469b5e78b964fe3ebbf7d935396
Ary <sehgalary@gmail.com>
2025-10-05 17:45:44 +0300

    Made a User Facorite Controller

---
+++ AryTickets/Controllers/UserFavoritesController.cs
@@ -0,0 +1,54 @@
+﻿using AryTickets.Data;
+using AryTickets.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AryTickets.Controllers
+{
+    [Authorize]
+    public class UserFavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserFavoritesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ToggleFavorite(int movieId, string movieTitle, string posterPath)
+        {
+            var userId = _userManager.GetUserId(User);
+            var existingFavorite = await _context.UserFavorites
+                .FirstOrDefaultAsync(f => f.UserId == userId && f.MovieId == movieId);
+
+            if (existingFavorite != null)
+            {
+                // It exists, so remove it
+                _context.UserFavorites.Remove(existingFavorite);
+            }
+            else
+            {
+                // It doesn't exist, so add it
+                var newFavorite = new UserFavorite
+                {
+                    UserId = userId,
+                    MovieId = movieId,
+                    MovieTitle = movieTitle,
+                    PosterPath = posterPath
+                };
+                _context.UserFavorites.Add(newFavorite);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", "Movie", new { id = movieId });
+        }
+    }
+}
