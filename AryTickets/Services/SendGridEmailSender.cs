f90c432ee17a953276d80fa2591043bc682e2128
Ary <sehgalary@gmail.com>
2025-10-05 20:40:20 +0300

    Email sending improvements

---
+++ AryTickets/Services/SendGridEmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace AryTickets.Services
+{
+    public class SendGridEmailSender : IEmailSender
+    {
+        private readonly string _apiKey;
+        private readonly string _fromEmail;
+        private readonly string _fromName;
+
+        public SendGridEmailSender(IConfiguration configuration)
+        {
+            _apiKey = configuration["EmailSettings:SendGridApiKey"];
+            _fromEmail = configuration["EmailSettings:FromEmail"];
+            _fromName = configuration["EmailSettings:FromName"];
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            if (string.IsNullOrEmpty(_apiKey))
+            {
+                throw new System.Exception("SendGrid API Key is not configured.");
+            }
+
+            var client = new SendGridClient(_apiKey);
+            var from = new EmailAddress(_fromEmail, _fromName);
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
