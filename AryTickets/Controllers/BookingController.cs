005cd4ef70ff6d6d27032bf21d8b1b1e5671ccdc
Ary <sehgalary@gmail.com>
2025-10-05 20:16:02 +0300

    Checkout file creation:

--- AryTickets/Controllers/BookingController.cs
+++ AryTickets/Controllers/BookingController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AryTickets.Controllers
 {
@@ -13,12 +14,41 @@ namespace AryTickets.Controllers
             var viewModel = new SeatSelectionViewModel
             {
                 MovieId = movieId,
-                MovieTitle = "The Epic Movie", 
+                MovieTitle = "The Epic Movie", // Placeholder title
                 Showtime = "October 26, 2025 - 8:00 PM",
                 SeatingChart = GenerateMockSeatingChart()
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(string movieTitle, string showtime, string selectedSeats, decimal totalPrice)
+        {
+            var viewModel = new CheckoutViewModel
+            {
+                MovieTitle = movieTitle,
+                Showtime = showtime,
+                SelectedSeats = selectedSeats,
+                TotalPrice = totalPrice
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProcessPayment(CheckoutViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid payment details.");
+            }
+
+            await Task.Delay(2500);
+
+            
+
+            return Json(new { success = true });
         }
 
         private List<List<Seat>> GenerateMockSeatingChart()
@@ -30,19 +60,11 @@ namespace AryTickets.Controllers
             for (int i = 0; i < rows.Length; i++)
             {
                 var row = new List<Seat>();
-                int seatCounter = 1; 
-
+                int seatCounter = 1;
                 for (int j = 1; j <= 14; j++)
                 {
-                    if (j == 7 || j == 8)
-                    {
-                        row.Add(null);
-                    }
-                  
-                    else if (rows[i] == 'H' && (j <= 2 || j >= 13))
-                    {
-                        row.Add(null); 
-                    }
+                    if (j == 7 || j == 8) { row.Add(null); }
+                    else if (rows[i] == 'H' && (j <= 2 || j >= 13)) { row.Add(null); }
                     else
                     {
                         var status = random.Next(1, 10) > 8 ? SeatStatus.Taken : SeatStatus.Available;
---
+++ AryTickets/Models/CheckoutViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AryTickets.Models
+{
+    public class CheckoutViewModel
+    {
+        public string MovieTitle { get; set; }
+        public string Showtime { get; set; }
+        public string SelectedSeats { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Name on Card")]
+        public string CardHolderName { get; set; }
+
+        [Required]
+        [CreditCard]
+        [Display(Name = "Card Number")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/?([0-9]{2})$", ErrorMessage = "Expiry date must be in MM/YY format.")]
+        [Display(Name = "Expiry Date (MM/YY)")]
+        public string ExpiryDate { get; set; }
+
+        [Required]
+        [StringLength(4, MinimumLength = 3)]
+        [Display(Name = "CVC")]
+        public string Cvc { get; set; }
+    }
+}
