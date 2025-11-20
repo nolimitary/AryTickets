2ec7c9c8c18e10affaa0a5945bc2c2368b45cb8f
Ary <sehgalary@gmail.com>
2025-11-20 08:50:18 +0200

    Created the documentation

---
+++ AryTickets/Migrations/20251113101812_proekt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AryTickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class proekt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
