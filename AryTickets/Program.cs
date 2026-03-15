92014b372f0c6f2e01c167ba356002ff887cf462
ary <sehgalary@gmail.com>
2026-03-15 19:19:24 +0200

    Switch to SQLite when LocalDB is not available

--- AryTickets/Program.cs
+++ AryTickets/Program.cs
@@ -8,8 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+if (connectionString != null && connectionString.Contains("localdb", StringComparison.OrdinalIgnoreCase))
+{
+    // Use SQLite on non-Windows platforms
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlite("Data Source=AryTixDb.db"));
+}
+else
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+}
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
