021d14ff247cb66bc5034b5b8d5c094f0381a53b
ary <sehgalary@gmail.com>
2026-03-15 19:49:16 +0200

    Add role support to Identity configuration

--- AryTickets/Program.cs
+++ AryTickets/Program.cs
@@ -21,6 +21,7 @@ else
 }
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.ConfigureApplicationCookie(options =>
