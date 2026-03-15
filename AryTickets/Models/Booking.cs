02d7c89edde0a87ed6fbd0018884587724c0b3d5
ary <sehgalary@gmail.com>
2026-03-15 19:49:48 +0200

    Create Booking model for storing ticket purchases

---
+++ AryTickets/Models/Booking.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AryTickets.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public string UserEmail { get; set; }
+        public string UserName { get; set; }
+
+        [Required]
+        public string MovieTitle { get; set; }
+
+        public string Showtime { get; set; }
+        public string Seats { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime BookedAt { get; set; } = DateTime.UtcNow;
+    }
+}
