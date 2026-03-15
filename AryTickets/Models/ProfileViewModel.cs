ced82fa933a842625dc0ba700586d3e5a22878c6
ary <sehgalary@gmail.com>
2026-03-15 20:43:25 +0200

    Add real data fields to ProfileViewModel

--- AryTickets/Models/ProfileViewModel.cs
+++ AryTickets/Models/ProfileViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 
 namespace AryTickets.Models
@@ -8,6 +8,14 @@ namespace AryTickets.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public DateTime JoinDate { get; set; }
-        public List<Badge> Badges { get; set; }
+        public int TicketCount { get; set; }
+        public int FavoritesCount { get; set; }
+        public int ReviewsCount { get; set; }
+        public decimal TotalSpent { get; set; }
+        public List<Booking> RecentBookings { get; set; } = new List<Booking>();
+        public bool IsCritic { get; set; }
+        public bool HasPendingApplication { get; set; }
+        public bool HasApplication { get; set; }
+        public List<Badge> Badges { get; set; } = new List<Badge>();
     }
 }
